4999dabf6618eff0c29c972a4d34d384b6169b06
mr-grixa <113786455+mr-grixa@users.noreply.github.com>
2023-02-15 23:05:26 +0300

    Добавьте файлы проекта.

---
+++ L1 визуализация графика/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace L1_визуализация_графика
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generate = new System.Windows.Forms.Button();
+            this.UpDownCout = new System.Windows.Forms.NumericUpDown();
+            this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGraf1 = new System.Windows.Forms.PictureBox();
+            this.vScrollBar1_25 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar1_75 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar1_50 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar2_50 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar2_75 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar2_25 = new System.Windows.Forms.VScrollBar();
+            this.pictureBoxGraf2 = new System.Windows.Forms.PictureBox();
+            this.comboBoxX = new System.Windows.Forms.ComboBox();
+            this.comboBoxY = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dX1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dX2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dY1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dY2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vScrollBar1_0 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar1_100 = new System.Windows.Forms.VScrollBar();
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownCout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraf1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraf2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(12, 12);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(131, 26);
+            this.generate.TabIndex = 0;
+            this.generate.Text = "Сгенерировать";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // UpDownCout
+            // 
+            this.UpDownCout.Location = new System.Drawing.Point(150, 17);
+            this.UpDownCout.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.UpDownCout.Name = "UpDownCout";
+            this.UpDownCout.Size = new System.Drawing.Size(120, 20);
+            this.UpDownCout.TabIndex = 1;
+            this.UpDownCout.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // pictureBoxMain
+            // 
+            this.pictureBoxMain.BackColor = System.Drawing.Color.White;
+            this.pictureBoxMain.Location = new System.Drawing.Point(12, 44);
+            this.pictureBoxMain.Name = "pictureBoxMain";
+            this.pictureBoxMain.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxMain.TabIndex = 2;
+            this.pictureBoxMain.TabStop = false;
+            // 
+            // pictureBoxGraf1
+            // 
+            this.pictureBoxGraf1.BackColor = System.Drawing.Color.White;
+            this.pictureBoxGraf1.Location = new System.Drawing.Point(316, 44);
+            this.pictureBoxGraf1.Name = "pictureBoxGraf1";
+            this.pictureBoxGraf1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBoxGraf1.TabIndex = 3;
+            this.pictureBoxGraf1.TabStop = false;
+            // 
+            // vScrollBar1_25
+            // 
+            this.vScrollBar1_25.Location = new System.Drawing.Point(336, 164);
+            this.vScrollBar1_25.Maximum = 109;
+            this.vScrollBar1_25.Name = "vScrollBar1_25";
+            this.vScrollBar1_25.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar1_25.TabIndex = 4;
+            this.vScrollBar1_25.Value = 75;
+            this.vScrollBar1_25.ValueChanged += new System.EventHandler(this.ScrolChanged1);
+            // 
+            // vScrollBar1_75
+            // 
+            this.vScrollBar1_75.Location = new System.Drawing.Point(376, 164);
+            this.vScrollBar1_75.Maximum = 109;
+            this.vScrollBar1_75.Name = "vScrollBar1_75";
+            this.vScrollBar1_75.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar1_75.TabIndex = 5;
+            this.vScrollBar1_75.Value = 25;
+            this.vScrollBar1_75.ValueChanged += new System.EventHandler(this.ScrolChanged1);
+            // 
+            // vScrollBar1_50
+            // 
+            this.vScrollBar1_50.Location = new System.Drawing.Point(356, 164);
+            this.vScrollBar1_50.Maximum = 109;
+            this.vScrollBar1_50.Name = "vScrollBar1_50";
+            this.vScrollBar1_50.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar1_50.TabIndex = 6;
+            this.vScrollBar1_50.Value = 50;
+            this.vScrollBar1_50.ValueChanged += new System.EventHandler(this.ScrolChanged1);
+            // 
+            // vScrollBar2_50
+            // 
+            this.vScrollBar2_50.Location = new System.Drawing.Point(506, 164);
+            this.vScrollBar2_50.Maximum = 109;
+            this.vScrollBar2_50.Name = "vScrollBar2_50";
+            this.vScrollBar2_50.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar2_50.TabIndex = 10;
+            // 
+            // vScrollBar2_75
+            // 
+            this.vScrollBar2_75.Location = new System.Drawing.Point(547, 164);
+            this.vScrollBar2_75.Maximum = 109;
+            this.vScrollBar2_75.Name = "vScrollBar2_75";
+            this.vScrollBar2_75.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar2_75.TabIndex = 9;
+            // 
+            // vScrollBar2_25
+            // 
+            this.vScrollBar2_25.Location = new System.Drawing.Point(464, 164);
+            this.vScrollBar2_25.Maximum = 109;
+            this.vScrollBar2_25.Name = "vScrollBar2_25";
+            this.vScrollBar2_25.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar2_25.TabIndex = 8;
+            // 
+            // pictureBoxGraf2
+            // 
+            this.pictureBoxGraf2.BackColor = System.Drawing.Color.White;
+            this.pictureBoxGraf2.Location = new System.Drawing.Point(464, 44);
+            this.pictureBoxGraf2.Name = "pictureBoxGraf2";
+            this.pictureBoxGraf2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBoxGraf2.TabIndex = 7;
+            this.pictureBoxGraf2.TabStop = false;
+            // 
+            // comboBoxX
+            // 
+            this.comboBoxX.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxX.FormattingEnabled = true;
+            this.comboBoxX.Items.AddRange(new object[] {
+            "X1",
+            "X2",
+            "Y1",
+            "Y2"});
+            this.comboBoxX.Location = new System.Drawing.Point(48, 250);
+            this.comboBoxX.Name = "comboBoxX";
+            this.comboBoxX.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxX.TabIndex = 11;
+            this.comboBoxX.SelectedIndexChanged += new System.EventHandler(this.comboBoxY_SelectedIndexChanged);
+            // 
+            // comboBoxY
+            // 
+            this.comboBoxY.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxY.FormattingEnabled = true;
+            this.comboBoxY.Items.AddRange(new object[] {
+            "X1",
+            "X2",
+            "Y1",
+            "Y2"});
+            this.comboBoxY.Location = new System.Drawing.Point(212, 123);
+            this.comboBoxY.Name = "comboBoxY";
+            this.comboBoxY.Size = new System.Drawing.Size(58, 21);
+            this.comboBoxY.TabIndex = 12;
+            this.comboBoxY.SelectedIndexChanged += new System.EventHandler(this.comboBoxY_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dX1,
+            this.dX2,
+            this.dY1,
+            this.dY2});
+            this.dataGridView1.Location = new System.Drawing.Point(570, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(258, 375);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dX1
+            // 
+            this.dX1.HeaderText = "X1";
+            this.dX1.Name = "dX1";
+            this.dX1.Width = 50;
+            // 
+            // dX2
+            // 
+            this.dX2.HeaderText = "X2";
+            this.dX2.Name = "dX2";
+            this.dX2.Width = 50;
+            // 
+            // dY1
+            // 
+            this.dY1.HeaderText = "Y1";
+            this.dY1.Name = "dY1";
+            this.dY1.Width = 50;
+            // 
+            // dY2
+            // 
+            this.dY2.HeaderText = "Y2";
+            this.dY2.Name = "dY2";
+            this.dY2.Width = 50;
+            // 
+            // vScrollBar1_0
+            // 
+            this.vScrollBar1_0.Location = new System.Drawing.Point(316, 164);
+            this.vScrollBar1_0.Maximum = 109;
+            this.vScrollBar1_0.Name = "vScrollBar1_0";
+            this.vScrollBar1_0.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar1_0.TabIndex = 14;
+            this.vScrollBar1_0.Value = 100;
+            this.vScrollBar1_0.ValueChanged += new System.EventHandler(this.ScrolChanged1);
+            // 
+            // vScrollBar1_100
+            // 
+            this.vScrollBar1_100.Location = new System.Drawing.Point(396, 164);
+            this.vScrollBar1_100.Maximum = 109;
+            this.vScrollBar1_100.Name = "vScrollBar1_100";
+            this.vScrollBar1_100.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar1_100.TabIndex = 15;
+            this.vScrollBar1_100.ValueChanged += new System.EventHandler(this.ScrolChanged1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 450);
+            this.Controls.Add(this.vScrollBar1_100);
+            this.Controls.Add(this.vScrollBar1_0);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBoxY);
+            this.Controls.Add(this.comboBoxX);
+            this.Controls.Add(this.vScrollBar2_50);
+            this.Controls.Add(this.vScrollBar2_75);
+            this.Controls.Add(this.vScrollBar2_25);
+            this.Controls.Add(this.pictureBoxGraf2);
+            this.Controls.Add(this.vScrollBar1_50);
+            this.Controls.Add(this.vScrollBar1_75);
+            this.Controls.Add(this.vScrollBar1_25);
+            this.Controls.Add(this.pictureBoxGraf1);
+            this.Controls.Add(this.pictureBoxMain);
+            this.Controls.Add(this.UpDownCout);
+            this.Controls.Add(this.generate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownCout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraf1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGraf2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generate;
+        private System.Windows.Forms.NumericUpDown UpDownCout;
+        private System.Windows.Forms.PictureBox pictureBoxMain;
+        private System.Windows.Forms.PictureBox pictureBoxGraf1;
+        private System.Windows.Forms.VScrollBar vScrollBar1_25;
+        private System.Windows.Forms.VScrollBar vScrollBar1_75;
+        private System.Windows.Forms.VScrollBar vScrollBar1_50;
+        private System.Windows.Forms.VScrollBar vScrollBar2_50;
+        private System.Windows.Forms.VScrollBar vScrollBar2_75;
+        private System.Windows.Forms.VScrollBar vScrollBar2_25;
+        private System.Windows.Forms.PictureBox pictureBoxGraf2;
+        private System.Windows.Forms.ComboBox comboBoxX;
+        private System.Windows.Forms.ComboBox comboBoxY;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dX1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dX2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dY1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dY2;
+        private System.Windows.Forms.VScrollBar vScrollBar1_0;
+        private System.Windows.Forms.VScrollBar vScrollBar1_100;
+    }
+}
+
---
+++ L1 визуализация графика/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace L1_визуализация_графика
+{
+    public partial class Form1 : Form
+    {
+        public double[] X1;
+        public double[] Y1;
+        public double[] X2;
+        public double[] Y2;
+        public Form1()
+        {
+            InitializeComponent();
+            generateX();
+            comboBoxX.SelectedIndex = 0;
+            comboBoxY.SelectedIndex = 1;
+            
+
+        }
+
+        public void generateX()
+        {
+            Random random = new Random();
+            X1 = new double[(int)UpDownCout.Value];
+            X2 = new double[(int)UpDownCout.Value];
+            Y1 = new double[(int)UpDownCout.Value];
+            Y2 = new double[(int)UpDownCout.Value];
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < UpDownCout.Value; i++)
+            {
+                X1[i] = random.NextDouble();
+                X2[i] = random.NextDouble();
+                dataGridView1.Rows.Add();
+                dataGridView1["dX1", i].Value = X1[i];
+                dataGridView1["dX2", i].Value = X2[i];
+            }
+
+        }
+
+        private void Draw(double[] X, double[] Y)
+        {
+            int size = pictureBoxMain.Size.Width;
+            Bitmap bitmap = new Bitmap(size, size);
+            for (int i = 0; i < X.Length; i++)
+            {
+                bitmap.SetPixel((int)(size * X[i]), (int)(size * Y[i]), Color.Black);
+            }
+            pictureBoxMain.Image = bitmap;
+
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            generateX();
+            Draw(X1, X2);
+            
+        }
+
+
+
+        private void DrawLine(double x1, double y1, double x2, double y2)
+        {
+            double k = (y2 - y1) / (x2 - x1);
+            double b = y1 - k * x1;
+            int size = pictureBoxGraf1.Size.Width;
+            Bitmap bitmap = new Bitmap(size, size);
+            for (int i = 0; i < 100; i++)
+            {
+                Y1[i] = k * X1[i] + b;
+                if (Y1[i] < 1 && Y1[i] > 0)
+                {
+                    bitmap.SetPixel((int)(X1[i] * size), (int)(Y1[i] * size), Color.Black);
+                }
+                dataGridView1["dY1", i].Value = Y1[i];
+
+            }
+            pictureBoxGraf1.Image = bitmap;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void ScrolChanged1(object sender, EventArgs e)
+        {
+            int size = pictureBoxGraf1.Size.Width;
+            Bitmap bitmap = new Bitmap(size, size);
+            LineUr ur1 = new LineUr(
+                (double)(100 - vScrollBar1_0.Value) / 100d,
+                (double)(100 - vScrollBar1_25.Value) / 100d,
+                (double)(100 - vScrollBar1_50.Value) / 100d,
+                (double)(100 - vScrollBar1_75.Value) / 100d,
+                (double)(100 - vScrollBar1_100.Value) / 100d
+                );
+            for (double i = 0; i < 1; i += 0.001d)
+            {
+                double y = ur1.GetY(i);
+                if (y < 1 && y > 0)
+                {
+                    bitmap.SetPixel((int)(i * size), (int)(y * size), Color.Black);
+                }
+            }
+            bitmap.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            pictureBoxGraf1.Image = bitmap;
+            for (int i = 0; i < X1.Length; i++)
+            {
+                Y1[i] = ur1.GetY(X1[i]);
+                dataGridView1["dY1", i].Value = Y1[i];
+            }
+            ref double[] refX = ref SwitchXY(comboBoxX.SelectedIndex);
+            ref double[] refY = ref SwitchXY(comboBoxY.SelectedIndex);
+            Draw(refX, refY);
+        }
+
+        private void comboBoxY_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ref double[] refX = ref SwitchXY(comboBoxX.SelectedIndex);
+            ref double[] refY = ref SwitchXY(comboBoxY.SelectedIndex);
+            Draw(refX, refY);
+        }
+        private ref double [] SwitchXY(int CB)
+        {
+            switch (CB)
+            {
+                case 0:
+                    return ref X1;
+                    break;
+                case 1:
+                    return ref X2;
+                    break;
+                case 2:
+                    return ref Y1;
+                    break;
+                case 3:
+                    return ref Y2;
+                    break;
+                default:
+                    return ref X1;
+            }
+        }
+
+    }
+}
+
+public class LineUr
+{
+    double k1;
+    double k2;
+    double k3;
+    double k4;
+    double b1;
+    double b2;
+    double b3;
+    double b4;
+    public LineUr(double y1, double y2, double y3, double y4, double y5)
+    {
+
+        k1 = (y2 - y1) / (0.25 - 0);
+        b1 = y1;
+
+        k2 = (y3 - y2) / (0.5 - 0.25);
+        b2 = y2 - k2 * 0.25;
+
+        k3 = (y4 - y3) / (0.75 - 0.5);
+        b3 = y3 - k3 * 0.5;
+
+        k4 = (y5 - y4) / (1 - 0.75);
+        b4 = y4 - k4 * 0.75;
+
+    }
+    public double GetY(double x)
+    {
+        double y;
+        if (x < 0.25)
+        {
+            y = k1 * x + b1;
+        }
+        else if (x < 0.5)
+        {
+            y = k2 * x + b2;
+        }
+        else if (x < 0.75)
+        {
+            y = k3 * x + b3;
+        }
+        else
+        {
+            y = k4 * x + b4;
+        }
+        return y;
+    }
+}
